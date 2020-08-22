b77b2fd66862817a021cfea5486f32e28f02f5db
rottenkitkat <williyantohuang97@yahoo.com>
2020-08-22 21:07:42 +0700

    added top navbar, responsive breakpoint and also the animation toggle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-var mySwiper = new Swiper(".swiper-container", {
+const navbar = document.querySelector("nav");
+const slide = document.querySelector(".swiper-container");
+const scrolled = document.querySelector(".scrolled");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight / 2) {
+    navbar.style.height = 0;
+  }
+  if (window.scrollY > window.innerHeight) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+    navbar.style.height = "50px";
+  }
+});
+
+const mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   effect: "fade",
   speed: 700,
